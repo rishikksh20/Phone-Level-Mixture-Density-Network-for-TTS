435406182bfcbdde322cfeac61230cecc08806bd
PranjalyaDS <pranjalya.tiwari@deepsync.co>
2021-11-23 12:55:37 +0000

    :zap: added evaluation

--- utils/util.py
+++ utils/util.py
@@ -18,7 +18,7 @@ import torch.nn.functional as F
 
 def get_mask_from_lengths(lengths):
     max_len = torch.max(lengths).item()
-    ids = torch.arange(0, max_len, out=torch.LongTensor(max_len))
+    ids = torch.arange(0, max_len, out=torch.LongTensor(max_len)).cuda()
     mask = (ids < lengths.unsqueeze(1)).bool()  # (B, max_len)
     return mask
 
